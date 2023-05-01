25e3dcece7c26964607599508ee52682ac2174ed
noeducation <noeducationvp@gmail.com>
2023-05-01 11:53:47 +0200

    Dodanie podstawowego kontrolera

---
+++ FootballNeighborhood.Api/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using FootballNeighborhood.Domain.Dtos.Common;
+using FootballNeighborhood.Infrastructure.Cqrs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballNeighborhood.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+    private readonly IDispatcher _dispatcher;
+
+    public BaseController(IDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    protected async Task<OperationResult<TResult>> DispatchAsync<TResult>(IQuery<TResult> query,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _dispatcher.SendAsync(query, cancellationToken);
+
+        if (!result.Success) Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        return result;
+    }
+
+    protected async Task<OperationResult> DispatchAsync(ICommand command, CancellationToken cancellationToken = default)
+    {
+        var result = await _dispatcher.SendAsync(command, cancellationToken);
+
+        if (!result.Success) Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        return result;
+    }
+}
